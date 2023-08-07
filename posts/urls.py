9e7e4d9c631d4aff8aa5360540dab3b7932ed67f
arm-path <arm.path.py@gmail.com>
2023-08-07 10:48:13 +0300

    edit and delete post

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import create_post, detail_post
+from .views import create, edit, delete, detail
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('create/', create_post, name='create'),
-    path('detail/<slug:user_slug>/title/<slug:slug>/', detail_post, name='detail')
+    path('create/', create, name='create'),
+    path('edit/<slug:slug>/', edit, name='edit'),
+    path('delete/<slug:slug>/', delete, name='delete'),
+    path('detail/<slug:user_slug>/title/<slug:slug>/', detail, name='detail')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.utils.text import slugify
 
 from .forms import PostForm
 from .models import Post
 
 
-def create_post(request):
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -15,9 +19,35 @@ def create_post(request):
             post.save()
     if request.method == 'GET':
         form = PostForm()
-    return render(request, 'posts/create.html', {'form': form})
+    return render(request, 'posts/create_edit.html', {'form': form, 'action': 'create'})
 
 
-def detail_post(request, user_slug, slug):
+@login_required
+@require_http_methods(['GET', 'POST'])
+def edit(request, slug):
+    post = get_object_or_404(Post, slug=slug, user=request.user)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.slug = slugify(post.title)
+            post.save()
+            return redirect(post.get_absolute_url())
+    if request.method == 'GET':
+        form = PostForm(instance=post)
+    return render(request, 'posts/create_edit.html', {'form': form, 'action': 'edit'})
+
+
+def delete(request, slug):
+    post = get_object_or_404(Post, slug=slug, user=request.user)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('profile:me')
+    return render(request, 'posts/delete.html', {'post': post})
+
+
+@login_required
+@require_http_methods(['GET'])
+def detail(request, user_slug, slug):
     post = get_object_or_404(Post, slug=slug, user__user_slug=user_slug)
     return render(request, 'posts/detail.html', {'post': post})
